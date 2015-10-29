0dbb9cccd54fa508d76649aa56f96a0c971f6841
hyung-uk <lhu114@nate.com>
2015-10-29 11:24:58 +0900

    [형욱 - 2015/10/29] 추가 : 삭제 : Test2, Test3 수정 : 코멘트 :

--- app/src/main/java/com/safering/safebike/Test.java
+++
@@ -1,7 +0,0 @@
-package com.safering.safebike;
-
-/**
- * Created by Tacademy on 2015-10-29.
- */
-public class Test {
-}
--- app/src/main/java/com/safering/safebike/test2.java
+++
@@ -1,7 +0,0 @@
-package com.safering.safebike;
-
-/**
- * Created by lhu on 2015-10-29.
- */
-public class test2 {
-}
