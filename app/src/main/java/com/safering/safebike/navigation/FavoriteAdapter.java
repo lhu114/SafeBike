88e02b8a51f1ac92f2216e86904a79dc8a3c14cc
hyung-uk <lhu114@nate.com>
2015-11-03 11:55:35 +0900

    Merge branch 'master' of https://github.com/lhu114/SafeBike
    # Conflicts:
    #	.idea/misc.xml
    #	app/app.iml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/safering/safebike/MainActivity.java
    
    [형욱 - 2015/11/03]
    추가 :
    삭제 :
    수정 :
    코멘트 : SharedPreferences 로 로직 수정 후 Back key 처리 완료(11:50)
    즐겨찾기 탭 수정 중

---
+++ app/src/main/java/com/safering/safebike/navigation/FavoriteAdapter.java
@@ -0,0 +1,50 @@
+package com.safering.safebike.navigation;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lhu on 2015-11-02.
+ */
+public class FavoriteAdapter extends BaseAdapter {
+    List<FavoriteItem> items =new ArrayList<FavoriteItem>();
+
+    public void add(FavoriteItem itemData) {
+        items.add(itemData);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        FavoriteItemView view;
+
+        if (convertView == null) {
+            view = new FavoriteItemView(parent.getContext());
+        } else {
+            view = (FavoriteItemView) convertView;
+        }
+
+        view.setItemData(items.get(position));
+
+        return view;
+    }
+}
