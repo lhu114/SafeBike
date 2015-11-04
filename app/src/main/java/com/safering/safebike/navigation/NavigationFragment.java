4ff358eb33b7d6ad4b41fa5578c2f6b32b43355f
hyung-uk <lhu114@nate.com>
2015-11-04 12:46:21 +0900

    Merge branch 'master' of https://github.com/lhu114/SafeBike
    # Conflicts:
    #	.idea/misc.xml
    #	app/app.iml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/safering/safebike/MainActivity.java
    
    [형욱 - 2015/11/04]
    추가 :
    삭제 :
    수정 :
    코멘트 : Map 진행 중 (네트워크 붙여서) 13:00

--- app/src/main/java/com/safering/safebike/navigation/NavigationFragment.java
+++ app/src/main/java/com/safering/safebike/navigation/NavigationFragment.java
@@ -144,32 +144,30 @@ public class NavigationFragment extends Fragment implements OnMapReadyCallback, 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        String poiName = data.getStringExtra(KEY_POI_NAME);
 
-        if (poiName != null) {
-            if (requestCode == REQUEST_SEARCH_POI && resultCode == Activity.RESULT_OK) {
-
+        if (requestCode == REQUEST_SEARCH_POI && resultCode == Activity.RESULT_OK) {
+            String poiName = data.getStringExtra(KEY_POI_NAME);
             Toast.makeText(getContext(), "NavigationFragment.onActivityResult.poiName : " + poiName, Toast.LENGTH_SHORT).show();
 //            activateDestination();
-                textView.setText(poiName);
+            textView.setText(poiName);
 
-                addressLayout.setVisibility(View.VISIBLE);
-                fabFindRoute.setVisibility(View.VISIBLE);
+            addressLayout.setVisibility(View.VISIBLE);
+            fabFindRoute.setVisibility(View.VISIBLE);
 
-                fabFindRoute.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getContext(), SelectRouteActivity.class);
-                        startActivity(intent);
-                    }
-                });
-            }
+            fabFindRoute.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getContext(), SelectRouteActivity.class);
+                    startActivity(intent);
+                }
+            });
         }
+    }
 
         /*
          * 실패 등 예외상황 처리
          */
-    }
+
 
 //    public void activateDestination() {
 //        addressLayout.setVisibility(View.VISIBLE);
